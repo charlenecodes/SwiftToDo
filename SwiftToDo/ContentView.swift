4b0845335eb5b44ba5bc44b4b941115ec7ccceb8
Charlene <cjlava17@yahoo.com>
2023-10-24 15:35:16 +0200

    Todo list with basic functionality

--- SwiftToDo/ContentView.swift
+++ SwiftToDo/ContentView.swift
@@ -8,19 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var taskList: [String] = []
+    @State var taskEntry: String = ""
+    @State var isActive = false
+    
     var body: some View {
-        HStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack{
+                if taskList.isEmpty {
+                    Spacer()
+                    Text("Enter a task below").padding(7)
+                    HStack{
+                        Text("Click the")
+                        Image(systemName: "plus.circle.fill")
+                        Text("to add a new task")
+                    }
+                    
+                    Spacer()
+                } else {
+                    List{
+                        ForEach(taskList, id: \.self) { task in
+                            HStack{
+                                Text(task)
+                                Spacer()
+                            }
+                        }
+                    }
+                    Spacer()
+                }
+                HStack {
+                    TextField("Enter a task", text: $taskEntry)
+                        .font(.headline)
+                        .foregroundColor(isActive ? Color(.purple) : Color(.lightGray))
+                        .padding(10)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(Color(.lightGray), lineWidth: 1.0)
+                        )
+                        .onTapGesture {
+                            if !isActive {
+                                isActive.toggle()
+                            }
+                        }
+                    Image(systemName: "plus.circle.fill")
+                        .onTapGesture {
+                            taskList.append(taskEntry)
+                            if isActive {
+                                isActive.toggle()
+                            }
+                            if taskEntry != "" {
+                                taskEntry.removeAll()
+                            }
+                            
+                        }
+                }
+                .padding()
+            }
+            .navigationTitle("To Do List 📝")
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
